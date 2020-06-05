db43527827c760579588f08dbe7de77d262cdf7a
ShityBrain <65502219+ShityBrainOfficial@users.noreply.github.com>
2020-06-05 17:33:07 +0200

    Update welcome.py

--- haruka/modules/welcome.py
+++ haruka/modules/welcome.py
@@ -80,9 +80,7 @@ def send(update, message, keyboard, backup_message):
             LOGGER.warning(message)
             LOGGER.warning(keyboard)
             LOGGER.exception("Could not parse! got invalid url host errors")
-            except BadRequest:
-          return ""
-       return msg
+            return msg
 
 @run_async
 def new_member(bot: Bot, update: Update):
